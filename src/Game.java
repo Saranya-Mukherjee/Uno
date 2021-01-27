c31c69e530e4e2bd4b67f3b085245db16940d39b
Saranya-Mukherjee <mukherjeesaranya722@gmail.com>
2021-01-27 17:01:02 +0530

    Uno

---
+++ src/Game.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class Game {
+
+    static Card[] cards =new Card[7];
+    static String[] types={"0","1","2","3","4","5","6","7","8","9","r","d","s"};
+
+
+    public static void getCards(){
+        Random random=new Random();
+        for(int i=0;i<cards.length;i++){
+            cards[i]=new Card(random.nextInt(6),types[random.nextInt(types.length)]);
+        }
+    }
+
+    public static void main(String[] args) {
+        getCards();
+        for (Card card : cards) {
+            System.out.println(card.getColor() +" "+card.getType());
+        }
+
+        Card top=new Card(0,"d");
+        Card[] found=top.findCards(cards);
+
+        System.out.println("============================");
+        for (Card card : found) {
+            System.out.println(card.getColor() +" "+card.getType());
+        }
+    }
+
+}
